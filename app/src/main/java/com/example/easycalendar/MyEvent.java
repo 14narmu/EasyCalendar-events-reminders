08e0055f0605717432d4d985e8bbae1ab86672ec
Caner Kaya <canerkaya718@gmail.com>
2020-05-16 23:28:28 +0300

    Update MyEvent.java

--- app/src/main/java/com/example/easycalendar/MyEvent.java
+++ app/src/main/java/com/example/easycalendar/MyEvent.java
@@ -2,16 +2,16 @@ package com.example.easycalendar;
 
 import android.location.Location;
 
-import com.prolificinteractive.materialcalendarview.CalendarDay;
+
 
 import org.threeten.bp.LocalDate;
 
-import java.util.Date;
+
 
 public class MyEvent {
     private String eventName;
     private int index_category;
-    private int color;
+    private int color =-6543440;
     private Time startTime;
     private Time endTime;
     private LocalDate startDate;
@@ -44,9 +44,12 @@ public class MyEvent {
         LocalDate date = LocalDate.of( Integer.valueOf(tokens[2]), Integer.valueOf(tokens[1]),Integer.valueOf(tokens[0]) );
         return date;
     }
-
-    public MyEvent(String eventName){
-        this.eventName  = eventName;
+    //TODO Delete after debug
+    public MyEvent(Time time,int index_category,String eventName, LocalDate startDate) {
+        this.index_category = index_category;
+        this.eventName = eventName;
+        this.startDate = startDate;
+        this.startTime = time;
     }
 
     public String getEventName() {
@@ -56,4 +59,111 @@ public class MyEvent {
     public void setEventName(String eventName) {
         this.eventName = eventName;
     }
+
+    public int getIndex_category() {
+        return index_category;
+    }
+
+    public void setIndex_category(int index_category) {
+        this.index_category = index_category;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getIndex_notification() {
+        return index_notification;
+    }
+
+    public void setIndex_notification(int index_notification) {
+        this.index_notification = index_notification;
+    }
+
+    public int getEmail_notification() {
+        return email_notification;
+    }
+
+    public void setEmail_notification(int email_notification) {
+        this.email_notification = email_notification;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isRememberEmail() {
+        return rememberEmail;
+    }
+
+    public void setRememberEmail(boolean rememberEmail) {
+        this.rememberEmail = rememberEmail;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public int getIndex_recurrance() {
+        return index_recurrance;
+    }
+
+    public void setIndex_recurrance(int index_recurrance) {
+        this.index_recurrance = index_recurrance;
+    }
+
+    public Location getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(Location eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+
+
 }
